36321666e5ebb150c71a7268799cae7756700ffa
BellaMrx <bella.mrx.pr0ject@gmail.com>
2023-12-04 19:36:16 +0100

    UD React

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,19 +1,25 @@
-import React, { Component } from "react";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  eventButton = (args) => {
-    alert(args + " : " + this);
-  };
-  render() {
-    return ( 
-      <div>
-        <h1> Trigger events in React </h1>
-        <button onClick = {
-            () => this.eventButton("One argument") 
-          } > Trigger event </button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
